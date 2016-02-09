bb05dac344ad7f61afeed302dea74c6394cd97e4
samar-ash <samarashrafi18@gmail.com>
2016-02-09 08:58:43 +0330

    Create fuel_js_lang.php

---
+++ modules/fuel/language/fuel_js_lang.php
@@ -0,0 +1,103 @@
+<?php 
+/*
+|--------------------------------------------------------------------------
+| Misc. For Javascript Files
+|--------------------------------------------------------------------------
+*/
+$lang['label_search'] = 'جستجو...';
+
+$lang['btn_save'] = 'ذخیره';
+$lang['btn_cancel'] = 'انصراف	';
+
+$lang['btn_add'] = 'اضافه کردن';
+$lang['btn_edit'] = 'ویرایش';
+
+$lang['btn_select'] = 'انتخاب';
+$lang['btn_asset'] = 'ابزار کمکی';
+$lang['btn_image'] = 'تصویر';
+$lang['btn_pdf'] = 'پی دی اف';
+$lang['btn_flash'] = 'فلش';
+$lang['btn_upload_asset'] = 'آپلود ابزار کمکی';
+$lang['btn_view_source'] = 'منبع';
+$lang['btn_view_editor'] = 'ویرایشگر';
+$lang['btn_preview'] = 'پیش نمایش';
+$lang['btn_select_all'] = 'انتخاب همه';
+$lang['btn_deselect_all'] = 'لغو انتخاب همه';
+
+$lang['comboselect_values_empty'] = 'هیچ مقداری برای انتخاب وجود ندارد';
+$lang['comboselect_selected_empty'] = 'از مقادیر سمت چپ صفحه، انتخاب نمایید';
+$lang['comboselect_filter'] = 'فیلتر کردن جستجو';
+$lang['tree_orphans'] = '_orphans';
+
+$lang['confirm_delete'] = 'آیا برای حذف مطمئن هستید؟';
+
+$lang['form_label_send_email_notification'] = 'آیا مایل به اطلاع رسانی تغییر پسورد از طریق ایمیل می باشید؟';
+
+$lang['error_importing_ajax'] = 'خطا در نمایش فایل';
+
+$lang['viewpage_close'] = 'خروج';
+$lang['viewpage_new_window'] = 'پنجره جدید';
+
+$lang['rearrange_on'] = 'شما می توانید با انتخاب و کشیدن ( درگ کردن) داده های زیر، اولویت را تغییر دهید';
+
+$lang['replace_warning'] = 'آیا مطمئید که می خواهید محتویات این قسمت را جایگزین کنید؟';
+
+$lang['edit_multi_select_warning'] = 'شما باید فقط یک گزینه انتخاب شده را ویرایش کنید.';
+
+$lang['filters_close'] = '&#8673; خروج';
+$lang['filters_open'] = '&#8675; باز کردن';
+
+
+/*
+|--------------------------------------------------------------------------
+| MarkItUp
+|--------------------------------------------------------------------------
+*/
+$lang['markitup_b'] = 'Bold';
+$lang['markitup_i'] = 'Italic';
+$lang['markitup_strike'] = 'Stroke Through';
+$lang['markitup_p'] = 'پاراگراف';
+$lang['markitup_h1'] = 'Heading 1';
+$lang['markitup_h2'] = 'Heading 2';
+$lang['markitup_h3'] = 'Heading 3';
+$lang['markitup_h4'] = 'Heading 4';
+$lang['markitup_ul'] = 'لیست نقطه ای';
+$lang['markitup_ol'] = 'لیست عددی';
+$lang['markitup_li'] = 'لیست آیتمی';
+$lang['markitup_blockquote'] = 'Block Quote';
+$lang['markitup_hr'] = 'Horizontal Rule';
+$lang['markitup_img'] = 'تصویر';
+$lang['markitup_link'] = 'لینک';
+$lang['markitup_target'] = 'هدف';
+$lang['markitup_title'] = 'عنوان';
+$lang['markitup_mailto'] = 'ایمیل به';
+$lang['markitup_php'] = 'متغیر PHP';
+$lang['markitup_clean'] = 'پاک کردن';
+$lang['markitup_preview'] = 'Preview Toggle (hold down alt/option and click to preview)';
+$lang['markitup_fullscreen'] = 'تمام صفحه';
+
+// placeholders
+$lang['markitup_placeholder_headings'] = 'عنوان موردنظر شما';
+$lang['markitup_placeholder_link'] = 'متن مورد نظر شما برای لینک کردن';
+$lang['markitup_email'] = 'ایمیل شما';
+$lang['markitup_fullscreen_close'] = 'خروج';
+
+
+/*
+|--------------------------------------------------------------------------
+| Repeatable
+|--------------------------------------------------------------------------
+*/
+$lang['warn_before_delete_msg'] = 'آیا برای حذف مطمئن هستید؟';
+$lang['btn_add_another'] = 'افزودن مورد دیگر';
+$lang['btn_remove'] = 'حذف کردن';
+
+/*
+|--------------------------------------------------------------------------
+| Assets
+|--------------------------------------------------------------------------
+*/
+$lang['assets_need_to_save'] = 'برای استفاده از این فایل دکمه ذخیره را بزنید';
+
+/* End of file fuel_js_lang.php */
+/* Location: ./modules/fuel/language/english/fuel_js_lang.php */
