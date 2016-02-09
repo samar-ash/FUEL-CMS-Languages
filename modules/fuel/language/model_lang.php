cb550e3aa9cb02fb460e34a604c3ba8985fa7314
samar-ash <samarashrafi18@gmail.com>
2016-02-09 09:00:48 +0330

    Create model_lang.php

---
+++ modules/fuel/language/model_lang.php
@@ -0,0 +1,23 @@
+<?php 
+// model specific error messages
+$lang['error_val_empty_or_already_exists'] = 'خالی است و یا وجود دارد %1s ';
+$lang['error_in_readonly_mode'] = 'در وضعیت فقط خواندنی است و نمی تواند ویرایش شود %1s ';
+
+// standard model errors
+$lang['error_required_fields'] = "لطفا فیلدهای خواسته شده %1s را وارد نمایید";
+$lang['error_valid_email'] = 'لطفا یک ایمیل معتبر وارد نمایید';
+$lang['error_valid_phone'] = 'لطفا یک شماره تلفن معتبر وارد نمایید';
+$lang['error_method_does_not_exist'] = "متد %1s وجود ندارد";
+$lang['error_class_does_not_exist'] = "کلاس %1s وجود ندارد";
+
+$lang['error_value_exceeds_length'] = 'از مقدار %1s تجاوز کرده است';
+$lang['error_not_number'] = 'مقدار برای %1s باید عدد باشد';
+$lang['error_invalid_date'] = 'تاریخ معتبری برای %1s نیست';
+$lang['error_invalid_time'] = 'زمان معتبری برای %1s نیست';
+$lang['error_invalid_year'] = 'سال معتبری برای %1s نیست';
+$lang['error_invalid_generic'] = 'مقدار معتبری برای %1s نیست';
+$lang['error_could_not_find_record_class'] = 'کلاس مورد نظر برای ساخت %1s یافت نمی شود';
+$lang['error_invalid_custom_class'] = 'کلاس مرتبط با فیلد %1sمعتبر نیست';
+
+/* End of file model_lang.php */
+/* Location: ./modules/fuel/language/english/model_lang.php */
